578fd8656c58d28fcee5ce47ca44d7f6023aab86
Bilal Wasim <bilalwasim676@gmail.com>
2021-10-17 18:52:55 +0100

    drivers/sensor: Adding buildable driver for bno080
    Adding driver that builds but does not yet provide any implementation
    for the HAL layer. A couple of warnings appear from within the bno080
    submodule which we may look at later.
    
    To build, add:
      .. CONFIG_SENSOR=y
      .. CONFIG_SPI=y
      .. CONFIG_BNO08X=y
    in your configuration file..
    
    Signed-off-by: Bilal Wasim <bilalwasim676@gmail.com>

---
+++ drivers/sensor/bno080/sh2_hal_impl.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2021 Entropic Engg
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_SENSOR_BNO080_H_
+#define ZEPHYR_DRIVERS_SENSOR_BNO080_H_
+
+#define SH2_HAL_MAX_TRANSFER (256)
+
+#endif /* ZEPHYR_DRIVERS_SENSOR_BNO080_H_ */
