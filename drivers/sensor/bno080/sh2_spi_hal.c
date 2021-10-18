677c5ed559c0149443be91b3e7a5aca79baa0d13
Bilal Wasim <bilalwasim676@gmail.com>
2021-10-18 23:27:21 +0100

    drivers/sensor: Adding more infrastructure for the BNO08x driver
    This commit adds the device tree binding, and basic init code for
    the BNO08x driver and a simple application to test all of this.
    
    The application just inits SH2 and tries to read the product IDs.
    More logic will be added in the app and the HAL as we make progress.
    The app logic has been copied from the Hillcrest example app with
    STM32: https://github.com/hcrest/bno080-nucleo-demo
    
    Though we have most of the infrastructure to support multiple instance
    in place, the HAL APIs exposed by the Hillcrest library doesn't
    expose parameters to support multi-instance support, and I imagine
    this is not important anyway.
    
    Signed-off-by: Bilal Wasim <bilalwasim676@gmail.com>

--- drivers/sensor/bno080/sh2_spi_hal.c
+++ drivers/sensor/bno080/sh2_spi_hal.c
@@ -4,7 +4,30 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#define DT_DRV_COMPAT hillcrest_bno08x
+
+#include <zephyr.h>
+#include <drivers/spi.h>
+#include <logging/log.h>
+#include <drivers/gpio.h>
+
 #include "sh2_hal.h"
+
+LOG_MODULE_REGISTER(BNO08X, CONFIG_SENSOR_LOG_LEVEL);
+
+struct bno08x_config {
+	char *bus_name;
+	struct gpio_dt_spec nrst;
+	struct gpio_dt_spec intn;
+	struct gpio_dt_spec wake;
+	struct gpio_dt_spec bootn;
+};
+
+static const struct device *spi;
+static struct spi_config spi_cfg;
+static const struct device *bno08x_dev;
+
+static struct k_sem sh2_lock;
 
 int sh2_hal_reset(bool dfuMode, sh2_rxCallback_t *onRx, void *cookie)
 {
@@ -23,10 +46,55 @@ int sh2_hal_rx(uint8_t *pData, uint32_t len)
 
 int sh2_hal_block(void)
 {
-	return 0;
+	return k_sem_take(&sh2_lock, K_FOREVER);
 }
 
 int sh2_hal_unblock(void)
 {
+	k_sem_give(&sh2_lock);
 	return 0;
 }
+
+static int sh2_spi_hal_init(const struct device *dev)
+{
+	const struct bno08x_config *config = dev->config;
+
+	spi = device_get_binding(config->bus_name);
+	if (spi == NULL) {
+		LOG_DBG("spi device not found: %s", config->bus_name);
+		return -EINVAL;
+	}
+
+	/* Set up the block/unblock lock. */
+	k_sem_init(&sh2_lock, 0, 1);
+
+	spi_cfg.operation = SPI_WORD_SET(8) | SPI_TRANSFER_MSB;
+	/* spi_cfg.frequency = config->spi_max_frequency; */
+
+	gpio_pin_set(config->nrst.port, config->nrst.pin, 0);
+	gpio_pin_set(config->bootn.port, config->bootn.pin, 1);
+	gpio_pin_set(config->wake.port, config->wake.pin, 1);
+
+	/* Function calls from the library do not contain information
+	 * about the device which means we need to store this info
+	 * globally. Logically this limits us to a single sensorhub
+	 * instance for now.. Shouldn't be a problem for now (or ever). */
+	bno08x_dev = dev;
+
+	return 0;
+}
+
+#define BNO08X_INIT(index)                                                    \
+	static const struct bno08x_config bno08x_config_##index = {               \
+		.bus_name = DT_INST_BUS_LABEL(index),                                 \
+		.nrst     = GPIO_DT_SPEC_INST_GET(index, nrst_gpios),                 \
+		.intn     = GPIO_DT_SPEC_INST_GET(index, intn_gpios),                 \
+		.wake     = GPIO_DT_SPEC_INST_GET(index, wake_gpios),                 \
+		.bootn    = GPIO_DT_SPEC_INST_GET(index, bootn_gpios),                \
+	};                                                                        \
+									                                          \
+	DEVICE_DT_INST_DEFINE(index, &sh2_spi_hal_init, NULL,                     \
+			    NULL, &bno08x_config_##index, POST_KERNEL,                    \
+			    CONFIG_SENSOR_INIT_PRIORITY, NULL);
+
+DT_INST_FOREACH_STATUS_OKAY(BNO08X_INIT)
